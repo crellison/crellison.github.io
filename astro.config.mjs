acbb60f5c42b4617a2206d9138f6b6e3080a5754
Cole Ellison <cole.ellison@gmail.com>
2025-03-01 15:31:06 -0500

    Update styles for code blocks

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,9 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://cole.ooo',
+  markdown: {
+    shikiConfig: {
+      theme: 'light-plus',
+    },
+  },
 });
